f01b0541089aac9396804bef0f9f1a614edca467
tangbq <tangbq@tiens.com>
2017-12-12 14:20:18 +0800

    update from dell msysgit

---
+++ controllers/SiteController.php
@@ -0,0 +1,40 @@
+<?php
+namespace app\modules\admin\controllers;
+
+use app\modules\admin\components\MenuHelper;
+use Yii;
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    public function actionMain()
+    {
+        return $this->render('//admin/site/main.tpl', [
+            'url' => [
+                'static' => '/statics/',
+            ],
+        ]);
+    }
+
+    public function actionIndex()
+    {
+        $this->layout = false;
+        $menu = MenuHelper::getAssignedMenu(Yii::$app->user->id);
+        // var_dump($menu);exit;
+        $userId = Yii::$app->user->identity->getId();
+        $userInfo = Yii::$app->authManager->getRolesByUser($userId);
+        // var_dump($userInfo);exit;
+
+        return $this->render('//admin/site/index.tpl', [
+            'url' => [
+                'static' => '/statics/',
+            ],
+            'menu' => $menu,
+            'userInfo' => [
+                'role' => key($userInfo),
+                'id' => Yii::$app->user->identity->getId(),
+                'name' => Yii::$app->user->identity->username,
+            ],
+        ]);
+    }
+}
