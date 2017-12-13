6bd6e47e0323538505eb3b60166d12cd10e62ee1
tangbq <tangbq@tiens.com>
2017-12-13 15:42:26 +0800

    update from dell msysgit

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,20 +3,10 @@ namespace app\modules\admin\controllers;
 
 use app\modules\admin\components\MenuHelper;
 use Yii;
-use yii\web\Controller;
 
-class SiteController extends Controller
+class SiteController extends \app\libs\AdminController
 {
 
-    public function beforeAction($action)
-    {
-        if (parent::beforeAction($action)) {
-            $this->layout = false;
-            return true;
-        }
-
-        return false;
-    }
     public function actionMain()
     {
         return $this->render('//admin/site/main.tpl', [
@@ -28,10 +18,6 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        // $action = Yii::$app->controller->module->module->requestedRoute;
-        // if (!Yii::$app->user->can('/' . $action)) {
-        //     throw new \yii\web\HttpException(403, 'No Auth');
-        // }
         $userId = Yii::$app->user->id;
         $menu = MenuHelper::getAssignedMenu($userId);
         $userInfo = Yii::$app->authManager->getRolesByUser($userId);
